5fe87cec551e8d31e3aa8424ed6ee774152d8e42
Craig McDonald <craig.g.mcdonald@gmail.com>
2017-10-24 10:16:01 -0500

    Hold for input fixed

--- EmgRTControl/EmgRTControl.cpp
+++ EmgRTControl/EmgRTControl.cpp
@@ -840,18 +840,18 @@ void EmgRTControl::sf_hold_for_input(const util::NoEventData* data) {
             }
             else if (is_testing()) {
 
-                    /*num_observations_per_class = is_any_num_key_pressed();
-                if (num_observations_per_class > 0) {
-                    present_more_targets = true;
-                }
-                else if (num_observations_per_class == 0) {
-                    finished = true;
-                }
+                /*num_observations_per_class = is_any_num_key_pressed();
+            if (num_observations_per_class > 0) {
+                present_more_targets = true;
             }
-            else {*/
+            else if (num_observations_per_class == 0) {
+                finished = true;
+            }
+        }
+        else {*/
                 util::print("ERROR: Condition number was set improperly. Going to ST_FAULT_STOP.");
                 auto_stop_ = true;
-            //}
+                }
         }
 
         // check for external input upon end of label sequence
