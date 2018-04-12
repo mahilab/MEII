48f2214d8664d652060b8014d224de31a4119390
Craig McDonald <craig.g.mcdonald@gmail.com>
2018-04-12 14:55:00 -0500

    adding python calling to direction classification

--- examples/ex_emg_direction_classification.cpp
+++ examples/ex_emg_direction_classification.cpp
@@ -10,9 +10,38 @@
 #include <MEL/Logging/DataLogger.hpp>
 #include <MEL/Core/Timer.hpp>
 #include <MEII/EMG/EmgDataCapture.hpp>
+#include <iostream>
+#include <fstream>
 
 using namespace mel;
 using namespace meii;
+
+bool read_csv(std::string filename, std::string directory, std::vector<std::vector<double>>& output) {
+	output.clear();
+	std::string full_filename = directory + "\\" + filename + ".csv";
+	std::ifstream input(full_filename);
+	input.precision(12);
+	if (input.is_open()) {
+		std::string csv_line;
+		while (std::getline(input, csv_line)) {
+			std::istringstream csv_stream(csv_line);
+			std::vector<double> row;
+			std::string number;
+			double data;
+			while (std::getline(csv_stream, number, ',')) {
+				std::istringstream number_stream(number);
+				number_stream >> data;
+				row.push_back(data);
+			}
+			output.push_back(row);
+		}
+		return true;
+	}
+	else {
+		LOG(Warning) << "File not found for read_csv().";
+		return false;
+	}
+}
 
 ctrl_bool stop(false);
 bool handler(CtrlEvent event) {
@@ -66,41 +95,46 @@ int main(int argc, char *argv[]) {
     MelShare ms_mes_tkeo_env("mes_tkeo_env");
     MelShare ms_mes_dm("mes_dm");
     MelShare ms_pred_label("pred_label");
+	MelShare ms_directory("file_path");
+	MelShare ms_filename("file_name");
+	MelShare ms_lda_training_flag("lda_training_flag");
    
 
     // create data log for EMG data
-    DataLogger emg_log(WriterType::Buffered, false);
-    std::vector<std::string> emg_log_header;
-    emg_log_header.push_back("Time [s]");
-    for (std::size_t i = 0; i < emg_channel_count; ++i) {
-        emg_log_header.push_back("MES DM " + stringify(emg_channel_numbers[i]));
-    }
-    emg_log.set_header(emg_log_header);
-    std::vector<double> emg_log_row(emg_log_header.size());
+    DataLogger training_log(WriterType::Buffered, false);
+    std::vector<double> training_log_row;
 
 
     // initialize testing conditions
     Time Ts = milliseconds(1); // sample period
-    std::size_t num_classes = 4; // number of active classes
+    std::size_t num_classes = 2; // number of active classes
     std::vector<Key> active_keys = { Key::Num1, Key::Num2, Key::Num3, Key::Num4 };
-    Time mes_active_capture_period = seconds(3);
+    Time mes_active_capture_period = seconds(1);
     Time mes_active_period = milliseconds(200);
     std::size_t mes_active_capture_window_size = (std::size_t)((unsigned)(mes_active_capture_period.as_seconds() / Ts.as_seconds()));
     mes.resize_buffer(mes_active_capture_window_size);
     std::size_t mes_active_window_size = (std::size_t)((unsigned)(mes_active_period.as_seconds() / Ts.as_seconds()));
     std::size_t pred_label = 0;
-    
+	std::string directory_ = "C:\\Users\\Ted\\GitHub\\MEII\\bin\\Release";
+	std::string filename_ = "S00_EFE_training_data";
+	std::string program_directory_ = "C:\\Users\\Ted\\GitHub\\MEII\\python";
+	std::vector<double> lda_training_complete(1, 0.0);
+	std::vector<std::vector<double>> training_data;
+	bool python_return = false;
+	std::vector<std::vector<double>> weights;
+	std::vector<std::vector<double>> lda_coeffs;
+	std::vector<double> lda_intercept;
 
     // initialize classifier
     bool RMS = true;
     bool MAV = true;
     bool WL = true;
-    bool ZC = false;
-    bool SSC = false;
-    bool AR1 = false;
-    bool AR2 = false;
-    bool AR3 = false;
-    bool AR4 = false;
+    bool ZC = true;
+    bool SSC = true;
+    bool AR1 = true;
+    bool AR2 = true;
+    bool AR3 = true;
+    bool AR4 = true;
     EmgDirClassifier dir_classifier(num_classes, emg_channel_count, Ts, RMS, MAV, WL, ZC, SSC, AR1, AR2, AR3, AR4);
     
     // enable DAQ
@@ -116,77 +150,116 @@ int main(int argc, char *argv[]) {
     // start while loop
     q8.watchdog.start();
 
-    // prompt the user for input
-    print("Press 'A + target #' to add training data for one target.");
-    print("Press 'C + target #' to clear training data for one target.");
-    print("Number of targets/classes is:");
-    print(num_classes);
-    print("Press 'T' to train classifier and begin real-time classification.");
-    print("Press 'Escape' to exit.");
+	// prompt the user for input
+	print("Press 'A + target #' to add training data for one target.");
+	print("Press 'C + target #' to clear training data for one target.");
+	print("Number of targets/classes is:");
+	print(num_classes);
+	print("Press 'T' to train classifier and begin real-time classification.");
+	print("Press 'Escape' to exit.");
 
-    while (!stop) {
+	while (!stop) {
 
-        // update all DAQ input channels
-        q8.update_input();
+		// update all DAQ input channels
+		q8.update_input();
 
-        // emg signal processing
-        mes.update_and_buffer();
-
-
-        // write to emg data log
-        emg_log_row = mes.get_demean();
-        emg_log_row.insert(emg_log_row.begin(), timer.get_elapsed_time().as_seconds());
-        emg_log.buffer(emg_log_row);
+		// emg signal processing
+		mes.update_and_buffer();
 
 
-        // predict state
+
+		// predict state
 		if (dir_classifier.update(mes.get_demean())) {
 			pred_label = dir_classifier.get_class();
 		}
 
-        // clear active data
-        for (std::size_t k = 0; k < num_classes; ++k) {
-            if (Keyboard::are_all_keys_pressed({ Key::C, active_keys[k] })) {
-                if (keypress_refract_clock.get_elapsed_time() > keypress_refract_time) {
-                    if (dir_classifier.clear_training_data(k)) {
-                        LOG(Info) << "Cleared active data for target " + stringify(k + 1) + ".";
-                    }
-                    keypress_refract_clock.restart();
-                }
-            }
-        }
+		// clear active data
+		for (std::size_t k = 0; k < num_classes; ++k) {
+			if (Keyboard::are_all_keys_pressed({ Key::C, active_keys[k] })) {
+				if (keypress_refract_clock.get_elapsed_time() > keypress_refract_time) {
+					if (dir_classifier.clear_training_data(k)) {
+						LOG(Info) << "Cleared active data for target " + stringify(k + 1) + ".";
+					}
+					keypress_refract_clock.restart();
+				}
+			}
+
+		}
 
 
 
 
-        // capture active data
-        for (std::size_t k = 0; k < num_classes; ++k) {
-            if (Keyboard::are_all_keys_pressed({ Key::A, active_keys[k] })) {
-                if (mes.is_buffer_full()) {
-                    if (keypress_refract_clock.get_elapsed_time() > keypress_refract_time) { 
-                        if (dir_classifier.add_training_data(k, find_sum_max_window(mes.get_tkeo_env_buffer_data(mes_active_capture_window_size), mes_active_window_size, mes.get_dm_buffer_data(mes_active_capture_window_size)))) {
-                            LOG(Info) << "Added active data for target " + stringify(k + 1) + ".";
-                        }
-                        keypress_refract_clock.restart();
-                    }
-                }
-            }
-        }
+		// capture active data
+		for (std::size_t k = 0; k < num_classes; ++k) {
+			if (Keyboard::are_all_keys_pressed({ Key::A, active_keys[k] })) {
+				if (mes.is_buffer_full()) {
+					if (keypress_refract_clock.get_elapsed_time() > keypress_refract_time) {
+						if (dir_classifier.add_training_data(k, find_sum_max_window(mes.get_tkeo_env_buffer_data(mes_active_capture_window_size), mes_active_window_size, mes.get_dm_buffer_data(mes_active_capture_window_size)))) {
+							LOG(Info) << "Added active data for target " + stringify(k + 1) + ".";
+						}
+						keypress_refract_clock.restart();
+					}
+				}
+			}
+		}
 
-        // train the active/rest classifiers
-        if (Keyboard::is_key_pressed(Key::T)) {
-            if (keypress_refract_clock.get_elapsed_time() > keypress_refract_time) {
-                if (dir_classifier.train()) {
-                    LOG(Info) << "Trained new active/rest classifier based on given data.";
-                }
-                keypress_refract_clock.restart();
-            }
-        }
+		// train the active/rest classifiers
+		if (Keyboard::is_key_pressed(Key::T)) {
+			if (keypress_refract_clock.get_elapsed_time() > keypress_refract_time) {
+				//if (dir_classifier.train()) {
+				//	LOG(Info) << "Trained new active/rest classifier based on given data.";
+				//}
+				dir_classifier.train();
+
+				print(dir_classifier.get_class_feature_data(0));
+
+				for (std::size_t i = 0; i < num_classes; ++i) {
+					training_data = dir_classifier.get_class_feature_data(i);
+					for (std::size_t j = 0; j < training_data.size(); ++j) {
+						training_log.buffer(training_data[j]);
+					}
+				}
+				training_log.save_data(filename_, directory_, false);
+
+
+
+				// send file location to python over melshare
+				ms_directory.write_message(directory_);
+				ms_filename.write_message(filename_);
+
+				// open LDA script in Python
+				std::string system_command;
+				system_command = "start " + program_directory_ + "\\" + "EMG_FS_LDA.py &";
+				system(system_command.c_str());
+
+				// wait for python to return results
+				print("Waiting for Python to return training results...");
+				
+			}
+		}
+
+		if (!lda_training_complete.empty() && lda_training_complete[0] == 1.0) {
+			if (!python_return) {
+				python_return = true;
+
+				read_csv("S00_EFE_emg_dir_classifier.csv", directory_, weights);
+				print(weights);
+				//for (std::size_t m = 0; m < weights.size(); ++m){
+				//	for (std::size_t n = 0; n, weights[0].size()-1; ++n) {
+				//		lda_coeffs[m][n] = weights[m][n];
+				//	}
+				//	lda_intercept[m] = weights[m][weights[0].size()];
+				//}
+
+				//	dir_classifier.set_model(lda_coeffs,lda_intercept);
+			}
+		}
 
         // write to MelShares
         ms_mes_tkeo_env.write_data(mes.get_tkeo_envelope());
         ms_mes_dm.write_data(mes.get_demean());
         ms_pred_label.write_data({ (double)((signed)(pred_label + 1)) });
+		lda_training_complete = ms_lda_training_flag.read_data();
 
         // check for exit key
         if (Keyboard::is_key_pressed(Key::Escape)) {
@@ -207,4 +280,3 @@ int main(int argc, char *argv[]) {
     disable_realtime();
     return 0;
 }
-
