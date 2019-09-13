7a5c0de88499353822d4b3e7910229aa3bce0831
nathandunk <nbd2@rice.edu>
2019-09-13 11:35:17 -0500

    Added .vscode for use with visual studio code, removed NI Crio stuff from this branch, and updated clean.py to work with python3

--- util/clean.py
+++ util/clean.py
@@ -1,15 +1,21 @@
+# Evan Pezent (epezent@rice.edu)
+# MEL Universal Clean Script
+# 7/2018
+
 # This script cleans the build and binary directories associated with MEII. If
 # a particular directory does not exist, it is created.
 
 import os
 import shutil
 
-keep = ['bin/Release/myo32.dll',
-        'bin/Release/myo64.dll']
+keep = ['bin/sftp-config.json',
+        'bin/NiFpga_quadrature.lvbitx',
+        'bin/Release/myo32.dll',
+        'bin/Release/myo64.dll',
+        'lib/sftp-config.json']
 
 folders = ['build',
            'bin',
-           'bin/Release',
            'lib']
 
 files = []
@@ -21,25 +27,25 @@ for folder in folders:
     try:
         files += [folder + "/" + f for f in os.listdir(folder)]
     except:
-        print 'Creating: ', folder[3:]
+        print('Creating: ' + folder[3:])
         os.mkdir(folder)
         files += [folder + "/" + f for f in os.listdir(folder)]
 
 files = [f for f in files if f not in folders + keep]
 
 if not files:
-    print 'Already Clean'
+    print('Already Clean')
 else:
     for file in files:
         try:
-            print 'Removing: ', file[3:]
+            print('Removing: ' + file[3:])
             os.remove(file)
         except:
             pass
         try:
-           print 'Removing: ', file[3:]
-           shutil.rmtree(file)
+            print('Removing: ' + file[3:])
+            shutil.rmtree(file)
         except:
             pass
-    print 'Finished Cleaning'
+    print('Finished Cleaning')
 
